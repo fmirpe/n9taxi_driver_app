aae2f37036c15106347f2401ef1b8e294d0483cd
Francisco Miranda <fmirpe@gmail.com>
2024-07-06 02:32:20 -0500

    Iconos y algunas mejoras

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 
+import 'firebase_options.dart';
 import 'providers/map_provider.dart';
 import 'providers/user_provider.dart';
 import 'screens/cash_earned_screen.dart';
@@ -14,19 +15,13 @@ import 'theme.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: 'AIzaSyBpyrJlOBgcTcOgCsZqqLv_gdq_7lUUi34',
-      appId: '1:31172218743:android:3b4501b0c94883837c5b72',
-      messagingSenderId: '31172218743',
-      projectId: 'n9driverapp',
-      storageBucket: 'n9driverapp.appspot.com',
-    ),
+    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const TaxiAppDriver());
 }
 
 class TaxiAppDriver extends StatelessWidget {
-  const TaxiAppDriver({Key? key}) : super(key: key);
+  const TaxiAppDriver({super.key});
 
   @override
   Widget build(BuildContext context) {
